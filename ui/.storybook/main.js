b3f788b4ac56359325509fcb73554dc4b2c7f827
Tyler Hooper <thepapeep@gmail.com>
2021-11-01 12:05:51 -0400

    disabled storyshots and playwright addon

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -1,15 +1,33 @@
+const { setConfig } = require("storybook-addon-playwright/configs");
+const playwright = require("playwright");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "storybook-addon-apollo-client",
-    "@storybook/addon-knobs/register",
-    "storybook-addon-playwright/preset",
-    "storybook-addon-playwright/register"
-  ]
-}
+    // "@storybook/addon-knobs/dist/register",
+    // "storybook-addon-playwright/preset",
+    // "storybook-addon-playwright/register",
+  ],
+};
+
+// (async () => {
+//   let browser = {
+//     chromium: await playwright["chromium"].launch({headless: false, slowMo: 50}),
+//     //firefox: await playwright["firefox"].launch(),
+//     //webkit: await playwright["webkit"].launch(),
+//   };
+//   setConfig({
+//     storybookEndpoint: `http://localhost:8081/`,
+//     getPage: async (browserType, options) => {
+//       const page = await browser[browserType].newPage(options);
+//       return page;
+//     },
+//     afterScreenshot: async (page) => {
+//       await page.close();
+//     },
+//   });
+// })();
--- ui/jest.config.ts
+++ ui/jest.config.ts
@@ -1,8 +1,7 @@
 const { defaults } = require("jest-config");
-
 module.exports = {
   // ...
-
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   // ...
 };
